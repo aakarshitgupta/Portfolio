fba1c204c2408f081450155270da3034bc039356
Akarshit <aakarshitgupta307@gmail.com>
2024-06-26 22:48:30 +0530

    added animation in text

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let banner = document.querySelector(".banner");
 daynight.addEventListener("click",()=>{
     banner.classList.toggle("night");
 })
+
+let typingeffect = new Typed("#text",{
+    strings:["Akarshit Gupta","Web Developer", "Web Designer"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1000
+})
